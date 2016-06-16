8ce5dfa39081f86861514bf0b591eba480a58a73
Sam Cate <SamCate@Sams-MacBook-Pro.local>
2016-06-16 17:04:17 -0600

    Signs in/Signs up

--- app.js
+++ app.js
@@ -90,55 +90,198 @@ app.get('/callback',
     //TODO:If New - Insert into DB, return user ID, set req.session.id
     //TODO:Else - get user ID - set req.session.id
     // app.use(session({ secret: 'YOUR_SECRET_HERE', resave: true,  saveUninitialized: false }));
-    console.log('1 = ' + req.session.user);
+    console.log('1 = ' + req.session.secret);
     // req.session.destroy;
     // console.log('2 = '+req.session.user);
+    // function createUser(req,res){
 
-    if (!req.session.user) {
-      // console.log('User' + req.user);
-      req.session.user = req.user;
-      console.log(req.session.user);
+
+    if (!req.session.secret) {
+      console.log('Didnt find existing session token');
       var accessToken =
-      JSON.stringify(req.user.identities[0].access_token) || JSON.stringify(req.session.identities[0].access_token),
+      JSON.stringify(req.user.identities[0].user_id) || JSON.stringify(req.session.identities[0].user_id),
 
-        id = JSON.stringify(req.user.identities[0].user_id),
+      email,
+      name;
+
+      gEmail = email;
+      gName = name;
+
+      req.session.secret = accessToken;
+      console.log('session token = ' + req.session.secret);
+
+
+        if(req.user.emails) {
+          console.log('Email is available')
+          email = JSON.stringify(req.user.emails[0].value);
+        } else {
+          console.log('Email is not available - creating email')
+          email = (JSON.stringify(req.user.name.familyName + req.user.name.givenName) + '@tag.com');
+        }
+
+        console.log('Email = ' + email);
+        // console.log(
+        //   'Name = ' + JSON.stringify(req.user.displayName),
+        //   'User id = ' + JSON.stringify(req.user.identities[0].user_id),
+        //   'Access Token = ' + JSON.stringify(req.user.identities[0].user_id)
+        // )
+        knex('users')
+        .where('email', email)
+        .first()
+        .then(function(data){
+          console.log(data);
+          if (!data) {
+            //TODO:Create User
+            console.log('Email didnt match - creating user')
+            req.session.secret = accessToken;
+            knex('user')
+            .insert({
+              email: email,
+              firstname: req.user.name.givenName,
+              lastname: req.user.name.familyName,
+              logintoken: accessToken,
+              password: '', // encrypt the pass for db storage
+              roles: JSON.stringify([enums.userRole[0]]),
+              loginprovider: req.user.provider
+            })
+            .catch(function(err){
+              console.log(err);
+            })
+          } else {
+            //TODO:Login user
+            console.log('Email existed, account verified - Signing In')
+            req.session.secret = accessToken;
+            req.session.user = data
+          }
+          // console.log(req.session.user);
+        })
+        .catch(function(e){
+          console.log(e);
+        })
+    } else {
+      if(req.session.secret === req.user.identities[0].user_id) {
+        console.log('Session found - same token');
+        knex('users')
+        .where('email', email)
+        .first()
+        .then(function(data){
+          console.log(data);
+        })
+        return;
+      } else {
+        console.log('Session found - different token');
+        var accessToken =
+        JSON.stringify(req.user.identities[0].user_id) || JSON.stringify(req.session.identities[0].user_id),
 
         email,
         name;
 
+        gEmail = email;
+        gName = name;
 
-      req.session.secret = accessToken;
+        req.session.secret = accessToken;
+        console.log('session token = ' + req.session.secret);
 
-      console.log('session = ' + req.session.secret)
 
-      if(req.user.emails) {
-        email = JSON.stringify(req.user.emails[0].value);
-      } else {
-        email = 'Not available';
+          if(req.user.emails) {
+            console.log('Email is available')
+            email = JSON.stringify(req.user.emails[0].value);
+          } else {
+            console.log('Email is not available')
+            email = (JSON.stringify(req.user.name.familyName + req.user.name.givenName) + '@tag.com');
+          }
+
+          console.log('Email = ' + email);
+
+        knex('users')
+        .where('email', email)
+        .first()
+        .then(function(data){
+          console.log(data);
+          if (!data) {
+            console.log('Email didnt match - creating user')
+            //TODO:Create User
+            req.session.secret = accessToken;
+            knex('user')
+            .insert({
+              email: email,
+              firstname: req.user.name.givenName,
+              lastname: req.user.name.familyName,
+              logintoken: accessToken,
+              password: '', // encrypt the pass for db storage
+              roles: JSON.stringify([enums.userRole[0]]),
+              loginprovider: req.user.provider
+            })
+            .catch(function(err){
+              console.log(err);
+            })
+          } else {
+            //TODO:Login user
+            console.log('Email existed - Signed In')
+            req.session.secret = accessToken;
+            req.session.user = data;
+            console.log(data);
+          }
+        })
       }
+      // createUser(req,res);
+    }
+    // }
+    // console.log(req.session.user);
+    // createUser(req,res);
+  // res.redirect("/user");
+  res.send();
+});
+
+    // if (!req.session.user) {
+    //   // console.log('User' + req.user);
+    //   // req.session.user = req.user;
+    //   // console.log(req.session.user);
+    //
+    //
+    //   var accessToken =
+    //   JSON.stringify(req.user.identities[0].user_id) || JSON.stringify(req.session.identities[0].user_id),
+    //
+    //     id = JSON.stringify(req.user.identities[0].user_id),
+    //
+    //     email,
+    //     name;
+    //
+    //
+    //   req.session.secret = accessToken;
+    //
+    //   console.log('session = ' + req.session.secret)
+    //
+    //   if(req.user.emails) {
+    //     email = JSON.stringify(req.user.emails[0].value);
+    //   } else {
+    //     email = 'Not available';
+    //   }
+    //
+    //
+    //   console.log('Email = ' + email);
+    //   console.log(
+    //     'Name = ' + JSON.stringify(req.user.displayName),
+    //     'User id = ' + JSON.stringify(req.user.identities[0].user_id),
+    //     'Access Token = ' + JSON.stringify(req.user.identities[0].access_token)
+    //   )
 
 
-      console.log('Email = ' + email);
-      console.log(
-        'Name = ' + JSON.stringify(req.user.displayName),
-        'User id = ' + JSON.stringify(req.user.identities[0].user_id),
-        'Access Token = ' + JSON.stringify(req.user.identities[0].access_token)
-      )
-      knex('users')
-      .insert({
-        email: email,
-        id: id.toString(),
-        firstname: req.user.name.givenName,
-        lastname: req.user.name.familyName,
-        logintoken: '', //input issue, causes error
-        tokenexpiry: 3600,
-        password: '', // encrypt the pass for db storage
-        roles: JSON.stringify([enums.userRole[0]]),
-        loginprovider: req.user.provider
-      })
-      .catch(function(err){
-        console.log(err);
-      })
+      // knex('users')
+      // .insert({
+      //   email: email,
+      //   // id: id.toString(),
+      //   firstname: req.user.name.givenName,
+      //   lastname: req.user.name.familyName,
+      //   logintoken: accessToken, //input issue, causes error
+      //   // tokenexpiry: '3600',
+      //   password: '', // encrypt the pass for db storage
+      //   roles: JSON.stringify([enums.userRole[0]]),
+      //   loginprovider: req.user.provider
+      // })
+      // .catch(function(err){
+      //   console.log(err);
+      // })
 
       // req.session.id = req.user.identities[0].user_id;
       // console.log('Session token ' + req.session.id)
@@ -162,12 +305,6 @@ app.get('/callback',
       // .catch(function(err){
       //   console.log(err);
       // })
-    } else {
-      console.log('existed');
-    }
-    // res.redirect("/user");
-    res.send();
-  });
 
 app.get('/user', function (req, res) {
   // console.log(req.user);
