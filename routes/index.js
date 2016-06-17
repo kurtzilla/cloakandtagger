72e417ff9050c842d8aae8b0757f3f8365ffeb76
J. Haskins <ziopads@gmail.com>
2016-06-17 11:30:25 -0600

    Fix dossier route and view to be dynamic

--- routes/index.js
+++ routes/index.js
@@ -18,9 +18,60 @@ router.get('/testgameplayhunt', function(req, res, next) {
   // console.log('Yo!');
   res.render('gameplay/includes/hunt');
 })
+// router.get('/testgameplaydossier', function(req, res, next) {
+//   res.render('gameplay/includes/dossier');
+// })
 router.get('/testgameplaydossier', function(req, res, next) {
-  res.render('gameplay/includes/dossier');
-})
+  console.log(req.session.user.id);
+  knex('players')
+  // .where({userid: req.session.user.id})
+  .where({userid: 2})
+  // .where({userid:_userid})
+  .orderBy('id', 'desc')
+  .first()
+  .then(function(currentplayer) {
+    if(currentplayer){
+
+      knex('activeplayers')
+      .where({gameid:currentplayer.gameid, playerid:currentplayer.id})
+      .first()
+      .then(function(activeplayer){
+
+        knex('players')
+        .where({id:activeplayer.targetid})
+        .first()
+        .then(function(targetedplayer){
+
+          knex('users')
+          .where({id:targetedplayer.userid})
+          .first()
+          .then(function(targeteduser){
+            console.log(targeteduser);
+            res.render('gameplay/includes/dossier', {target: targeteduser});
+            // now access you vars via targeteduser (imageurl) and targetedplayer(lastlocation)
+
+
+          })
+          .catch(function(err){
+            next(err);
+          });
+        })
+        .catch(function(err){
+          next(err);
+        });
+      })
+      .catch(function(err){
+        next(err);
+      });
+    } else {
+      console.log('player IS NOT member of game');
+      res.send(targetlocale);
+    }
+  })
+  .catch(function(err){
+    next(err);
+  });
+});
 router.get('/testgameplaygame', function(req, res, next) {
   res.render('gameplay/includes/game');
 })
